4b514a1772aed26136078493cd8cc7a6c0e739b3
softwareTM <beisenov_b_b@mail.ru>
2018-11-19 06:18:31 +0300

    Change requests timeout

--- YandexMapGeojsonDescriptorBot/YandexMapGeojsonDescriptorBot.py
+++ YandexMapGeojsonDescriptorBot/YandexMapGeojsonDescriptorBot.py
@@ -15,7 +15,7 @@ class BotHandler:
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
         self.file_api_url = "https://api.telegram.org/file/bot{}/".format(token)
 
-    def get_updates(self, offset=None, timeout=30):
+    def get_updates(self, offset=None, timeout=None):
         method = 'getUpdates'
         params = {'timeout': timeout, 'offset': offset}
         resp = requests.get(self.api_url + method, params)
