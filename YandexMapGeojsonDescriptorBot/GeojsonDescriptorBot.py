6e05a7624d97fec700bc6c9874497b197215bb83
softwaretm <beisenov_b_b@mail.ru>
2018-11-26 10:53:55 +0300

    Put code temporarily into local development mode (get auth from config.ini, get updates through GetUpdates); add multiple elif;

--- YandexMapGeojsonDescriptorBot/GeojsonDescriptorBot.py
+++ YandexMapGeojsonDescriptorBot/GeojsonDescriptorBot.py
@@ -3,17 +3,18 @@ from os import environ
 from telegram.ext import Updater
 import requests
 import configparser
+import json
 
 # Initialize project values from a config file
-#config = configparser.ConfigParser()
-#config.read('config.ini')
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 my_token = environ.get('Token', '1111')
 # later used to create a webhook
 default_port = environ.get('Port', '1111')
 my_port = int(os.environ.get('PORT', default_port))
 my_webapp = environ.get('WebAppLink', 'catch')
-#my_token = config['Telegram.API']['Token']
+my_token = config['Telegram.API']['Token']
 #default_port = config['Webhook']['Port']
 #my_port = config['Webhook']['Port'] # int(environ['Port'])
 #my_webapp = config['Webhook']['WebAppLink'] #environ['WebAppLink']  
@@ -32,9 +33,6 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 ##
 ### Function definitions
 
-def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
-
 def count_objects_in_geojson(my_geojson):
     num_of_polygons = 0
     num_of_lines = 0
@@ -43,9 +41,9 @@ def count_objects_in_geojson(my_geojson):
     for feature in my_geojson['features']:
         if feature['geometry']['type'] == 'Polygon':
             num_of_polygons += 1
-        if feature['geometry']['type'] == 'LineString':
+        elif feature['geometry']['type'] == 'LineString':
             num_of_lines += 1
-        if feature['geometry']['type'] == 'Point':
+        elif feature['geometry']['type'] == 'Point':
             num_of_points += 1
     
     return num_of_polygons, num_of_lines, num_of_points
@@ -61,6 +59,24 @@ def document_parsing(bot, update):
 
         num_of_polygons, num_of_lines, num_of_points = count_objects_in_geojson(my_received_json)
         message_text = 'Количество многоугольников в созданной карте: {}, количество линий: {}, количество меток: {}.'.format(num_of_polygons, num_of_lines, num_of_points)
+    #from io import StringIO
+    #output = StringIO()
+    #output.write('First line.\n')
+    #bot.send_document(chat_id=update.message.chat_id, document=output)
+    #output.close()
+
+    #data = {'sender':   'Alice',
+    #'receiver': 'Bob',
+    #'message':  'We did it!'}
+    #import json
+    #data_json = json.dumps(data)
+    #bot.send_document(chat_id=update.message.chat_id, document=json.JSONEncoder(data))
+    #with open(new_file) as file:
+    #    file.write(message_text)
+    #    file.close()
+    #    bot.send_document(chat_id=update.message.chat_id, document=new_file.getvalue())
+        #os.remove('result.txt')
+
         with open("result.txt", "w") as file:
             file.write(message_text)
             file.close()
@@ -68,12 +84,12 @@ def document_parsing(bot, update):
             os.remove('result.txt')
         
         
-        # bot.send_message(chat_id=update.message.chat_id, text=message_text)
-    except:
-        bot.send_message(chat_id=update.message.chat_id, text="Некорректный файл.")
+         #bot.send_message(chat_id=update.message.chat_id, text=message_text)
+    except json.decoder.JSONDecodeError:
+        bot.send_message(chat_id=update.message.chat_id, text="Некорректный формат.")
+    except KeyError:
+        bot.send_message(chat_id=update.message.chat_id, text="Этот JSON файл не был создан в Конструкторе карт Яндекса.")
 
-def prompter(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Отправьте файл geojson, созданный в конструкторе карт Яндекса.")
 
 
 ##
@@ -83,9 +99,25 @@ from telegram.ext import CommandHandler
 from telegram.ext import MessageHandler, Filters
 
 # handles first activation of the bot by sending out a greeting
+def start(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Добрый день! \nЯ считаю количество многоугольников, линий и меток, находящихся в файле формата .geojson, созданном в Конструкторе карт Яндекса. В ответ вы получите файл .json с соответствующими значениями. \nДля подробной инструкции введите команду /help.")
+
 start_handler = CommandHandler('start', start)
 # we add this handler to the dispatcher so it becomes active
 dispatcher.add_handler(start_handler)
+
+def help(bot, update):
+    help_message = ('Я считаю количество многоугольников, линий и меток, находящихся в файле формата .geojson, '
+    'созданном в Конструкторе Карт от Яндекс. В ответ вы получите файл .json с соответствующими значениями.'
+    '\nСсылка на Конструктор карт: https://yandex.ru/map-constructor/.\nПосле создания своей карты, сохраните'
+    ' ее и экспортируйте в формате GEOJSON. После этого отправьте ее мне в чат приложенным документом.')
+    bot.send_message(chat_id=update.message.chat_id, text=help_message)
+
+help_handler = CommandHandler('help', help)
+dispatcher.add_handler(help_handler)
+
+def prompter(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="Отправьте файл geojson, созданный в конструкторе карт Яндекса.")
 
 prompter_handler = MessageHandler(Filters.text, prompter)
 dispatcher.add_handler(prompter_handler)
@@ -94,10 +126,15 @@ document_handler = MessageHandler(Filters.document, document_parsing)
 dispatcher.add_handler(document_handler)
 
 
-# Create a webhook to monitor updates from users
-updater.start_webhook(listen="0.0.0.0",
-                      port=my_port,
-                      url_path=my_token)
-updater.bot.set_webhook(my_webapp + my_token)
+# get updates through GetUpdates (useful for local testing)
+updater.start_polling()
 updater.idle()
+
+
+# Create a webhook to monitor updates to the API
+#updater.start_webhook(listen="0.0.0.0",
+#                      port=my_port,
+#                      url_path=my_token)
+#updater.bot.set_webhook(my_webapp + my_token)
+#updater.idle()
 
